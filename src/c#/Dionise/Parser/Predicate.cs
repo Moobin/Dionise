571c976c0c38af47870c8082a980154c983a5d49
joel <joel@nimue.(none)>
2012-08-25 14:32:08 -0300

    C# development branch. Parser algorithm.

---
+++ src/c#/Dionise/Parser/Predicate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dionise.Parser
+{
+    public class Predicate
+    {
+        public Languages Language { get; set; }
+        public ObjectTypes ObjectType { get; set; }
+        public string ObjectName { get; set; }
+        public string SchemaName { get; set; }
+        public string ReturnClause { get; set; }
+        public object ReturnType { get; set; }
+        public List<Parameter> Parameters { get; set; }
+
+        public Predicate(Languages language)
+        {
+            Language = language;
+            Parameters = new List<Parameter>();
+        }
+    }
+}
